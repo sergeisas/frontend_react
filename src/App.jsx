291e46d6716c063705d50d87bf59fd33abed0499
Pekka Naatsaari <pekka.naatsaari@hotmail.com>
2024-09-30 01:23:11 +0300

    todolist poisto ja pilkkominen

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TodoTable.jsx
@@ -0,0 +1,35 @@
+
+
+import PropTypes from 'prop-types';
+
+const TodoTable = ({ todos, onDelete }) => {
+  return (
+      <table>
+          <thead>
+              <tr>
+                  <th>Description</th>
+                  <th>Date</th>
+                  <th>Action</th>
+              </tr>
+          </thead>
+          <tbody>
+              {todos.map((todo, index) => (
+                  <tr key={index}>
+                      <td>{todo.description}</td>
+                      <td>{todo.date}</td>
+                      <td>
+                          <button onClick={() => onDelete(index)}>Delete</button>
+                      </td>
+                  </tr>
+              ))}
+          </tbody>
+      </table>
+  );
+}
+  
+  TodoTable.propTypes = {
+    todos: PropTypes.array.isRequired,
+    onDelete: PropTypes.func.isRequired
+  };
+
+  export default TodoTable;
